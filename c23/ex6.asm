207f2710c0a78d3acefa6bca146e22b7c216e2f1
Antonio Zea Jr <halosprt117@gmail.com>
2021-01-06 01:02:24 -0500

    Not functioning yet

--- c23/ex6.asm
+++ c23/ex6.asm
@@ -1,54 +1,50 @@
         .text
         .globl  main
 
-main:   li          $v0, 4
-        la          $a0, promptK
+main:   li              $v0, 4
+        la              $a0, promptK
         syscall
 
-        li          $v0, 5
+        li              $v0, 5
         syscall
         # $t0 is K
-        move        $t0, $v0
+        move            $t0, $v0
 
-        li          $v0, 4
-        la          $a0, promptN
+        li              $v0, 4
+        la              $a0, promptN
         syscall
 
-        li          $v0, 5
+        li              $v0, 5
         syscall
         # $t1 is N
-        move        $t1, $v0
+        move            $t1, $v0
 
         # $t4 is the accumulator for the numerator
-        move        $t4, $t1
+        li              $t4, 1
         # $t5 is the accumulator for the numerator
-        move        $t5, $t0
-        
-        
-        
+        li              $t5, 1
 
-
-        li          $t2, 1
-
-loop:   sltu        $t3, $t2, $t0
-        beq         $t3, $zero, endLp
+        
+loop:   beq             $t0, $zero, endLp
         nop
 
-        
+        mul             $t4, $t4, $t1
+        subu            $t1, $t1, 1
+        nop
+
+        mul             $t5, $t5, $t0
+        subu            $t0, $t0, 1
+        nop
+
+        j               loop
+        nop
+
+endLp:  nop
 
 
 
-        subu        $t1, $t1, 1
-        addu        $t2, $t2, 1
-        j           loop
-
-endLp:
-
-
-
-
-
-
+        li              $v0, 1
+        divu            $a0, $t4, $t5
 
 
 end:    li          $v0, 10
