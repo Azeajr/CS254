3d7b17bc72fa3bf9b3a37c4f11035af5c74661b2
Antonio Zea Jr <halosprtn117@gmail.com>
2021-02-08 20:58:37 -0500

    UI to access square and triangle subroutines

--- c27/ex3.asm
+++ c27/ex3.asm
@@ -2,12 +2,51 @@
         .globl main
 
 main:
-        li          $a0, 8
-        jal         square
+        li          $v0, 4
+        la          $a0, prompt1
+        syscall
 
-        li          $a0, 6
+        li          $v0, 8
+        la          $a0, buffer
+        li          $a1, 3
+        syscall
+
+        lb          $a0, ($a0)
+        beq         $a0, 0x53,  sqr
+
+        beq         $a0, 0x54, tri
+
+        j           main
+
+
+sqr:    li          $v0, 4
+        la          $a0, prompt2
+        syscall
+
+        li          $v0, 5
+        syscall
+
+        move        $a0, $v0
+
+        jal         square
+        j           end
+
+tri:    li          $v0, 4
+        la          $a0, prompt2
+        syscall
+
+        li          $v0, 5
+        syscall
+
+        move        $a0, $v0
+
         jal         triangle
 
-        li          $v0, 10
+end:    li          $v0, 10
         syscall
+
+        .data
+prompt1:.asciiz "Choose (S)quare or (T)riangle: "
+prompt2:.asciiz "Enter size: "
+buffer: .space  3
 
