084f5e605c4a68dbb296c12e8a005c1fa2c33079
Antonio Zea Jr <halosprtn117@gmail.com>
2021-02-16 20:20:09 -0500

    Documented and fully functional

--- c28/ex1.asm
+++ c28/ex1.asm
@@ -1,5 +1,6 @@
-
-
+# Translate the following program from C to MIPS
+# Focused on the use of frame linkage to setup variables in the run-time stack
+#
 #   main(){
 #     int f;        // f:   0($fp)
 #     int c;        // c:   4($fp)
@@ -14,22 +15,31 @@
         .globl main
 
 main:
+        # Main Prologue
+        # Push $ra and $fp onto stack.
         sub     $sp, $sp, 4
         sw      $ra, ($sp)
         sub     $sp, $sp, 4
         sw      $fp, ($sp)
 
+        # Initialize frame pointer
+        # $fp = $sp - space_for_variables
         sub     $fp, $sp, 8
+        # Initialize stack pointer so that it points to $fp
         move    $sp, $fp
 
+        # f = 50
         li      $t0, 50
         sw      $t0, 0($fp)
 
+        # toCent(f)
         lw      $a0, 0($fp)
         jal     toCent
-
+        
+        # c = toCent(f)
         sw      $v0, 4($fp)
 
+        # Prints the value of temp in fahrenheit
         li      $v0, 4
         la      $a0, promptF
         syscall
@@ -38,7 +48,7 @@ main:
         lw      $a0, 0($fp)
         syscall
 
-
+        # Prints the value of temp in celsius
         li      $v0, 4
         la      $a0, promptC
         syscall
@@ -47,12 +57,18 @@ main:
         lw      $a0, 4($fp)
         syscall
 
+
+        # Reset the stack pointer to the state tjat main found it in
         add     $sp, $fp, 8
 
+        # Pop the frame pointer from the stack
         lw      $fp, ($sp)
         add     $sp, $sp, 4
+        # Pop the return address from the stack
         lw      $ra, ($sp)
         add     $sp, $sp, 4
+
+        # Return control to caller
         jr      $ra
 
 
