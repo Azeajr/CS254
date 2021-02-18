6cd4ad15b882340ac3b5159f3980297521cb65f7
Antonio Zea Jr <halosprtn117@gmail.com>
2021-02-17 19:33:56 -0500

    First draft

---
+++ c28/Triangle.asm
@@ -0,0 +1,44 @@
+        .text
+        .globl  Triangle
+
+Triangle:
+
+        sub     $sp, $sp, 4
+        sw      $ra, ($sp)
+        sub     $sp, $sp, 4
+        sw      $fp, ($sp)
+        sub     $sp, $sp, 4
+        sw      $s0, ($sp)
+
+        sub     $fp, $sp, 4
+        move    $sp, $fp
+
+
+
+        move    $s0, $a0
+
+        bgt     $a0, 1, recurse
+        li      $v0, 1
+        j       Epilogue
+
+        
+
+
+
+recurse:
+        sub     $a0, $a0, 1
+        jal     Triangle
+
+        add     $v0, $v0, $s0
+
+Epilogue:
+        add     $sp, $fp, 4
+
+        lw      $s0, ($sp)
+        add     $sp, $sp, 4
+        lw      $fp, ($sp)
+        add     $sp, $sp, 4
+        lw      $ra, ($sp)
+        add     $sp, $sp, 4
+
+        jr      $ra
---
+++ c28/ex2.asm
@@ -0,0 +1,57 @@
+        .text
+        .globl  main
+
+main:
+        sub     $sp, $sp, 4
+        sw      $ra, ($sp)
+        sub     $sp, $sp, 4
+        sw      $fp, ($sp)
+
+        sub     $fp, $sp, 0
+
+        move    $sp, $fp
+
+        li      $v0, 4
+        la      $a0, prompt1
+        syscall
+
+        li      $v0, 5
+        syscall
+
+        move    $s0, $v0
+
+        li      $v0, 4
+        la      $a0, prompt2
+        syscall
+
+        li      $v0, 1
+        move    $a0, $s0
+        syscall
+
+        li      $v0, 4
+        la      $a0, prompt3
+        syscall
+
+
+        move    $a0, $s0
+        jal Triangle
+        move    $v0, $a0
+        li      $v0, 1
+        syscall
+
+
+        add     $sp, $fp, 0
+        # Pop the frame pointer from the stack
+        lw      $fp, ($sp)
+        add     $sp, $sp, 4
+        # Pop the return address from the stack
+        lw      $ra, ($sp)
+        add     $sp, $sp, 4
+
+        jr      $ra
+
+        .data
+prompt1:.asciiz "Enter a N: "
+prompt2:.asciiz "\nTriangle( "
+prompt3:.asciiz " ) = "
+newline:.asciiz "\n"
