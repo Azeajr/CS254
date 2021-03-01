7ea07afee19f322d65290f2a3049e37d0da016f5
Antonio Zea Jr <halosprtn117@gmail.com>
2021-02-28 20:26:40 -0500

    Used floats in expression

---
+++ c31/ex1.asm
@@ -0,0 +1,79 @@
+# Write a program that computes the value of the following arithmetic 
+# expression for values of x and y entered by the user:
+#
+# 5.4xy - 12.3y + 18.23x - 8.23
+
+        .text
+        .globl main
+        
+main:
+        sub     $sp, $sp, 4
+        sw      $ra, ($sp)
+        sub     $sp, $sp, 4
+        sw      $fp, ($sp)
+
+        # x:    0($fp)
+        # y:    4($fp)
+        sub     $fp, $sp, 8
+        move    $sp, $fp
+        
+        la      $a0, prompt1
+        li      $v0, 4
+        syscall
+
+        li      $v0, 6
+        syscall
+        s.s     $f0, 0($fp)
+
+        la      $a0, prompt2
+        li      $v0, 4
+        syscall
+
+        li      $v0, 6
+        syscall
+        s.s     $f0, 4($fp)
+
+
+        li.s    $f0, 5.4
+        l.s     $f1, 0($fp)
+        mul.s  $f0, $f1, $f0
+
+        l.s     $f1, 4($fp)
+        mul.s  $f12, $f1, $f0
+
+        li.s    $f0, -12.3
+        l.s     $f1, 4($fp)
+        mul.s  $f0, $f1, $f0
+
+        add.s   $f12, $f12, $f0
+        
+        li.s    $f0, 18.23
+        l.s     $f1, 0($fp)
+        mul.s  $f0, $f1, $f0
+
+        add.s   $f12, $f12, $f0
+
+        li.s      $f0, -8.23
+        add.s   $f12, $f12, $f0
+
+        la      $a0, prompt3
+        li      $v0, 4
+        syscall
+
+        li      $v0, 2
+        syscall
+
+        add     $sp, $fp, 8
+
+        lw      $fp, ($sp)
+        add     $sp, $sp, 4
+
+        lw      $ra, ($sp)
+        add     $sp, $sp, 4
+
+        jr      $ra
+
+        .data
+prompt1:.asciiz "Enter a float\nx: "
+prompt2:.asciiz "Enter a float\ny: "
+prompt3:.asciiz "Result: "
