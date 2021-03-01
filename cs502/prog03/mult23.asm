1674d720906edd86a58d67c5af7809ebfed222ed
Antonio Zea Jr <halosprtn117@gmail.com>
2021-02-28 21:31:55 -0500

    Fully commented.

--- cs502/prog03/mult23.asm
+++ cs502/prog03/mult23.asm
@@ -5,30 +5,37 @@
 ##  Programmer: Antonio Zea Jr
 ##  Date: 2021-02-28
 ##############################################################
+##  Register Table
+##  Reg         Use
+##  $7:         x
+##  $10:        Accumulator & Final Result
+##############################################################
         .text
         .globl main
 main:
-        # First draft using 3 registers
-        ori     $7, $0, 100
-        sll     $10, $7, 4
-        
-        sll     $8, $7, 2
-        addu    $10, $10, $8
+        # First draft used 3 registers
+        # Second draft uses 2 registers. Used Horner's Method to compute answer.
+        ori     $7, $0, 100     # Loads $7 with a 100. This is our x.
 
-        sll     $8, $7, 1
-        addu    $10, $10, $8
+        sll     $10, $7, 2      # Shifts $7 bitwise left by 2 and stores result 
+                                # in $10. This is equivalent to multiplying $7 
+                                # by 4.
 
-        addu    $10, $10, $7
-        # Second draft using 2 registers. Used Horner's Method to compute answer.
-        ori     $7, $0, 100
-        sll     $10, $7, 2
-        addu    $10, $10, $7
+        addu    $10, $10, $7    # Adds $7 to $10. This adds x back to 4x.
 
-        sll     $10, $10, 1
-        addu    $10, $10, $7
+        sll     $10, $10, 1     # Shifts $10 bitwise left by 1 and stores 
+                                # result back into $10. This multiplies 4x+x by 
+                                # 2. So $10 holds 8x+2x.
+                                
+        addu    $10, $10, $7    # Adds $7 to $10. This adds x back to 8x+2x.
 
-        sll     $10, $10, 1
-        addu    $10, $10, $7
+        sll     $10, $10, 1     # Shifts $10 bitwise left by 1 and stores 
+                                # result back into $10. This multiplies 8x+2x+x 
+                                # by 2. So $10 holds 16x+4x+2x.
+
+        addu    $10, $10, $7    # Adds $7 to $10. This adds x back to 
+                                # 16x+4x+2x. So $10 holds 16x+4x+2x+x which 
+                                # equals 23x.
 
 
 
