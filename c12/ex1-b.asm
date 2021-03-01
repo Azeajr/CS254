9ba2041823ffdf4b7d215dd0f55e70236890b3ed
Antonio Zea Jr <halosprtn117@gmail.com>
2021-03-01 04:54:01 -0500

    Comments

--- c12/ex1-b.asm
+++ c12/ex1-b.asm
@@ -1,6 +1,9 @@
 # Put the following bit pattern into register $1 :
 # DEADBEEF
-# Do this using just three instructions.
+# Do this one letter at a time, using ori to load each letter (each nibble) 
+# into a register, then shifting it into position. You will need to use ori, 
+# or, and sll instructions. Look at the contents of register $1 as you push F10 
+# to single step the program.
 
 		.text
 		.globl main
