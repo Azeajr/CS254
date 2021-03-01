6d89f1765d241d998930fce87d324e999a73754f
Antonio Zea Jr <halosprtn117@gmail.com>
2021-02-28 21:35:38 -0500

    Comments edited for relevance

--- cs502/prog03/mult23.asm
+++ cs502/prog03/mult23.asm
@@ -17,25 +17,19 @@ main:
         # Second draft uses 2 registers. Used Horner's Method to compute answer.
         ori     $7, $0, 100     # Loads $7 with a 100. This is our x.
 
-        sll     $10, $7, 2      # Shifts $7 bitwise left by 2 and stores result 
-                                # in $10. This is equivalent to multiplying $7 
-                                # by 4.
+        sll     $10, $7, 2      # This is equivalent to multiplying $7 by 4.
 
         addu    $10, $10, $7    # Adds $7 to $10. This adds x back to 4x.
 
-        sll     $10, $10, 1     # Shifts $10 bitwise left by 1 and stores 
-                                # result back into $10. This multiplies 4x+x by 
-                                # 2. So $10 holds 8x+2x.
-                                
+        sll     $10, $10, 1     # This multiplies 4x+x by 2. So $10 holds 8x+2x.
+
         addu    $10, $10, $7    # Adds $7 to $10. This adds x back to 8x+2x.
 
-        sll     $10, $10, 1     # Shifts $10 bitwise left by 1 and stores 
-                                # result back into $10. This multiplies 8x+2x+x 
-                                # by 2. So $10 holds 16x+4x+2x.
+        sll     $10, $10, 1     # This multiplies 8x+2x+x by 2. So $10 holds
+                                # 16x+4x+2x.
 
-        addu    $10, $10, $7    # Adds $7 to $10. This adds x back to 
-                                # 16x+4x+2x. So $10 holds 16x+4x+2x+x which 
-                                # equals 23x.
+        addu    $10, $10, $7    # This adds x back to 16x+4x+2x. So $10 holds 
+                                # 16x+4x+2x+x which equals 23x.
 
 
 
