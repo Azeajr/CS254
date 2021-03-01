b693974a70674761a95180561d76d5c3c3be3707
Antonio Zea Jr <halosprtn117@gmail.com>
2021-03-01 04:17:41 -0500

    Code & comments for clarity

--- c09/ex3.asm
+++ c09/ex3.asm
@@ -1,7 +1,14 @@
+# Notice: assembly instructions can use decimal or hex to specify bit patterns. 
+# In the below, 15 and 0xF result in the same bit pattern.
+
 		.text
 		.globl main
 
 main:
-		ori		$8,$0,0xf
-		ori		$9,$0,0x7
-		addu	$10,$8,$9
+		ori		$8, $0, 0xf
+		ori		$9, $0, 0x7
+		addu	$10, $8, $9
+
+		ori		$8, $0, 15
+		ori		$9, $0, 7
+		addu	$10, $8, $9
