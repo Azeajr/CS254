cb3799a72bbfe82ed21e71f38eefda5e77890c95
Antonio Zea Jr <halosprtn117@gmail.com>
2021-03-19 16:37:00 -0400

    Comments and formatting

--- c32/exp.asm
+++ c32/exp.asm
@@ -20,11 +20,11 @@ exp:
         sub     $sp, $sp, 4
         s.s     $f4, ($sp)
 
-        li.s    $f1, 1.0e-4
-        li.s    $f0, 1.0    # loop counter/divisor
-        li.s    $f2, 1.0    # constant 1.0
-        li.s    $f3, 1.0    # term
-        li.s    $f4, 1.0    # sum
+        li.s    $f1, 1.0e-4     # Accuracy of Taylor Series approximation
+        li.s    $f0, 1.0        # loop counter/divisor
+        li.s    $f2, 1.0        # constant 1.0
+        li.s    $f3, 1.0        # term
+        li.s    $f4, 1.0        # sum
         
 
 loopA:  c.lt.s  $f3, $f1
