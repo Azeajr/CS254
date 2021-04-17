cec3546093c1cc214d90d324575cb5830384cdf1
Antonio Zea Jr <halosprtn117@gmail.com>
2021-04-17 11:13:26 -0400

    Commented

--- c32/pi.asm
+++ c32/pi.asm
@@ -1,44 +1,43 @@
 #################################################################
-# $a0 = N
+# Arguments
+# $a0 = N 
 #################################################################
-# $f0 = numerator       0($fp)
-# $f2 = denominator     8($fp)
-# $f4 = term            16($fp)
-# $f6 = sum             24($fp)
+# 0($fp) = numerator       
+# 8($fp) = denominator     
+# 16($fp) = sum             
 #################################################################
+# Return values
 # $f0 = Return value
 #################################################################
         .text
         .globl pi
 
 pi:
-        #sub     $sp, $sp, 8
-        #s.d     $f0, ($sp)
-        sub     $sp, $sp, 8
-        s.d     $f2, ($sp)
-        #sub     $sp, $sp, 8
-        #s.d     $f12, ($sp)
-        sub     $sp, $sp, 4
+        sub     $sp, $sp, 8     # Store any bitpatterns currently in $f2. $f2 is
+        s.d     $f2, ($sp)      # going to be used as a double float so 8 bytes 
+                                # are needed.
+
+        sub     $sp, $sp, 4     # Store the current frame pointer
         sw      $fp, ($sp)
 
-        sub     $fp, $sp, 32
+        sub     $fp, $sp, 24    # Make enough space for 3 double variables
         move    $sp, $fp
 
         li.d    $f0, 1.0
-        s.d     $f0, 0($fp)
-        s.d     $f0, 8($fp)
+        s.d     $f0, 0($fp)     # numerator = 1.0
+        s.d     $f0, 8($fp)     # denominator = 1.0
         li.d    $f0, 0.0
-        s.d     $f0, 24($fp)
+        s.d     $f0, 16($fp)    # sum = 0.0
 
 loop:   beqz    $a0, endLp
 
         l.d     $f0, 0($fp)     # numerator
         l.d     $f2, 8($fp)     # denominator
-        div.d   $f0, $f0, $f2   # term
-        #s.d     $f0, 16($fp)
-        l.d     $f2, 24($fp)    # sum
+        div.d   $f0, $f0, $f2   # $f0 = term
+
+        l.d     $f2, 16($fp)    # sum
         add.d   $f2, $f2, $f0   # sum = sum + term
-        s.d     $f2, 24($fp)    # store sum
+        s.d     $f2, 16($fp)    # store sum
 
         l.d     $f0, 0($fp)     # numerator
         neg.d   $f0, $f0        # change sign of numerator
@@ -54,21 +53,16 @@ loop:   beqz    $a0, endLp
         j       loop
 endLp:
         li.d    $f2, 4.0
-        l.d     $f0, 24($fp)
-        mul.d   $f0, $f0, $f2
+        l.d     $f0, 16($fp)    # $f0 = sum
+        mul.d   $f0, $f0, $f2   # $f0 = 4 * sum
 
-        add     $sp, $fp, 32
+        add     $sp, $fp, 24    # Restore stack pointer since variables are no 
+                                # longer needed.
 
-        lw      $fp, ($sp)
+        lw      $fp, ($sp)      # Restore the old frame pointer
         add     $sp, $sp, 4
 
-        #l.d     $f12, ($sp)
-        #add     $sp, $sp, 8
-
-        l.d     $f2, ($sp)
+        l.d     $f2, ($sp)      # Restore the old value of $f2
         add     $sp, $sp, 8
 
-        #l.d     $f0, ($sp)
-        #add     $sp, $sp, 8
-
-        jr      $ra
+        jr      $ra             # Return to the calling subroutine
