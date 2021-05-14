8eb13d20da90a66fbee48ccfd2ac523c6f72683f
Antonio Zea Jr <halosprtn117@gmail.com>
2021-05-14 12:13:33 -0400

    Two linked lists with print subroutine

---
+++ c34/printList.asm
@@ -0,0 +1,38 @@
+        .text
+        .globl printList
+printList:
+        sub     $sp, $sp, 4
+        sw      $s0, ($sp)
+
+        move    $s0, $a0
+
+loopA:  beqz    $s0, endLPA
+        lw      $a0, ($s0)
+        li      $v0, 1
+        syscall
+
+        la      $a0, space
+        li      $v0, 4
+        syscall
+
+        lw      $s0, 4($s0)
+        j       loopA
+
+endLPA: la      $a0, newline
+        li      $v0, 4
+        syscall
+
+        lw      $s0, ($sp)
+        add     $sp, $sp, 4
+
+        li      $v0, 0          # Return Code
+
+        jr      $ra
+
+
+
+        .data
+space:  .asciiz " "
+newline:.asciiz "\n"
+
+
